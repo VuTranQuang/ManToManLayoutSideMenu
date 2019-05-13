61c4dbf8112b01546c8e68d68e5fa98086e63fab
Vu <vutran7993@gmail.com>
2019-05-13 15:06:31 +0700

    Initial Commit

---
+++ ManToManLayoutSideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManToManLayoutSideMenu
+//
+//  Created by Vu on 5/13/19.
+//  Copyright © 2019 Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
